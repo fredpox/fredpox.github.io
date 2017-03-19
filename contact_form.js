b0673142fa7dfe59d791bb14a4d57b3145e6f8fa
fredpox <fredpox@yahoo.it>
2017-03-19 14:07:43 +0100

    Update contact_form.js

--- contact_form.js
+++ contact_form.js
@@ -14,8 +14,8 @@ $.post("contact_form.php", {
 name1: name,
 email1: email,
 message1: message,
-contact1: contact
-}, function(data) {
+contact1: contact},
+function(data) {
 $("#returnmessage").append(data); // Append returned message to message paragraph.
 if (data == "Your Query has been received, We will contact you soon.") {
 $("#form")[0].reset(); // To reset form fields on success.
